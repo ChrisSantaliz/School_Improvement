30a143bf3dcd24baec7554affee3c17d01dc3036
Chris Santaliz <chris.santaliz@gmail.com>
2016-12-17 18:04:38 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var issues = [] 
+issues.push (issue)
+
+$("#submit").click(function() {
+   var schoolName =  ("#schoolName").val();
+   var issueDescription =  ("#issues").val();
+   var issue = {schoolname:schoolName , issuesDescription: issueDescription};
+}
+
+
