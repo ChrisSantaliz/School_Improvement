a06259aa0c9c465ef3d453301452fec90c63f5ed
Chris Santaliz <chris.santaliz@gmail.com>
2016-12-17 21:05:29 +0000

    fourth commit

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
- var issues = [{"schoolName":"Queens voc","issueDescription":"Trash","timestamp":1482001117419,"id":"issue1","votes":0},{"schoolName":"Queens voc","issueDescription":"Trash","timestamp":1482001119651,"id":"issue2","votes":0},{"schoolName":"Queens voc","issueDescription":"Trash","timestamp":1482001119818,"id":"issue3","votes":0}]
+ var issues = [{"schoolName":"Queens Voc","issueDescription":"New Bathrooms","timestamp":1482007922868,"id":"issue1","votes":0}]
  
  $(document).ready(function(){
    
 
-
-
-   
    $("#submit").click(function(event){
       var schoolName =  $("#schoolName").val();
       var issueDescription =  $("#issues").val();
-      var issue = {schoolname:schoolName , 
-      issuesDescription: issueDescription ,
+      var issue = {schoolName:schoolName , 
+      issueDescription: issueDescription ,
       timestamp: new Date() .getTime() ,
       id: "issue" + (issues.length + 1) ,
       votes : 0
@@ -18,37 +15,46 @@
       };
       issues.push(issue);
       console.log(issues);
-      updatelist() 
+      updatelists() 
    });
-
-   $("#issuelist").on("click","button",function(event) {
+   $(".issuelist").on("click","button",function(event) {
     // get the issue id
     var id = $(this).attr("data-id") ;
     // loop through the issues
     $.each (issues,function(index,issue){
      //increase votes by one
-     
     if (issue.id == id ) {
      issue.votes= issue.votes+1
     }
-    
     })
-    
-     updatelist()
+     updatelists()
      console.log (id)
    })
-
-     updatelist() 
+     updatelists() 
 });
-
-function updatelist() {
-    $("#issuelist").html("")
+function updatelist(listname) {
+    $(listname).html("")
      $.each(issues,
       function (index, issue) {
+          console.log(issue)
           var html= "<div>" + issue.schoolName + "</div>";
           html += "<p>" + issue.issueDescription + "</p>";
           html += "<button data-id=\""+issue.id+"\"> Vote on this issue : " + issue.votes + "</button>";
-          $("#issuelist").append(html)
+          $(listname).append(html)
       });
 }
-
+function updatelists () {
+    issues.sort (sortfortop)
+    updatelist ("#toplist")
+    issues.sort (sortingbyrecent)
+    updatelist ("#recentlist")
+    
+}
+function sortfortop (issueA,issueB) {
+   return issueB.votes-issueA.votes
+   
+}
+function sortingbyrecent (issueA,issueB) { 
+   return issueB.timestamp-issueA.timestamp
+}
+  
