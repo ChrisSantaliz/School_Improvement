4bcfbf7117b873ffcea551e9f55d85bc8a893106
Chris Santaliz <chris.santaliz@gmail.com>
2016-12-17 21:50:22 +0000

    final page

--- script.js
+++ script.js
@@ -4,20 +4,19 @@
    
 
    $("#submit").click(function(event){
-      var schoolName =  $("#schoolName").val();
-      var issueDescription =  $("#issues").val();
+      var schoolName =  $("#school-field").val();
+      var issueDescription =  $("#problem-field").val();
       var issue = {schoolName:schoolName , 
       issueDescription: issueDescription ,
       timestamp: new Date() .getTime() ,
       id: "issue" + (issues.length + 1) ,
       votes : 0
-         
-      };
+      }
       issues.push(issue);
       console.log(issues);
       updatelists() 
    });
-   $(".issuelist").on("click","button",function(event) {
+   $(".section").on("click","button",function(event) {
     // get the issue id
     var id = $(this).attr("data-id") ;
     // loop through the issues
@@ -37,17 +36,27 @@ function updatelist(listname) {
      $.each(issues,
       function (index, issue) {
           console.log(issue)
-          var html= "<div>" + issue.schoolName + "</div>";
-          html += "<p>" + issue.issueDescription + "</p>";
-          html += "<button data-id=\""+issue.id+"\"> Vote on this issue : " + issue.votes + "</button>";
+          var html = ' <div class="problem"> '+
+         '<button class="button" data-id="'+issue.id+'">vote</button>' +
+        ' <h2 class="score">' + issue.votes + '</h2>'+
+        ' <p class= "field-title">Problem</p> '+
+         '<h2 class="problem-name"><span class="description">' + issue.issueDescription +  '</h2>'+
+        '<p class= "field-title">School</p> '+
+        '<h2 class="school-name">' + issue.schoolName + '</h2>'+
+         
+   ' </div> '
+         // var html= "<div>" + issue.schoolName + "</div>";
+         // html += "<p>" + issue.issueDescription + "</p>";
+          
+         // html += "<button data-id=\""+issue.id+"\"> Vote on this issue : " + issue.votes + "</button>";
           $(listname).append(html)
       });
 }
 function updatelists () {
     issues.sort (sortfortop)
-    updatelist ("#toplist")
+    updatelist ("#top-issues")
     issues.sort (sortingbyrecent)
-    updatelist ("#recentlist")
+    updatelist ("#recent-issues")
     
 }
 function sortfortop (issueA,issueB) {
